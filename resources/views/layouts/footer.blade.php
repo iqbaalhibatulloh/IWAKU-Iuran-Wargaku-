3ad5e8530b2370ee9ac8515b2c4b1912b0e105e7
iqbaalhibatulloh <iqbaalhibatulloh03@gmail.com>
2023-12-11 14:12:04 +0700

    11/12/2023

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 {{-- <footer class="footer text-center pb-2 pt-2 fixed-bottom bg-[#8d7b68]" style="margin-top: 5px;  fixed;" ><p><i><h5><p >Copyright &copy; IwaKu! 2023. All rights reserved.</p></h5></i></p></footer> --}}
 
-<div class="text-center pb-2 pt-2 bg-[#8d7b68] w-full fixed">
+<div class="text-center py-5 bg-[#8d7b68] w-full text-white fixed bottom-0">
     Copyright &copy; IwaKu! 2023. All rights reserved.
 </div>
